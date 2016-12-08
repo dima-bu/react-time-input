48f6fc4ccdd37c73be8bdab8b47b02f2f9f7a3f9
Gautam Mehra <node.enthu@gmail.com>
2016-12-08 15:21:27 -0800

    adding onFocus onBlur and name props as optional

--- example/index.jsx
+++ example/index.jsx
@@ -4,8 +4,12 @@ import TimeInput from './../src/timeInput.jsx';
 
 
 var TimeWrapper = React.createClass({
-
-
+	onFocusHandler:function(event){
+		console.log("hello there you entered :  my name is ",event.target.name);
+	},
+	onBlurHandler:function(event){
+		console.log("you left ");
+	},
 	onTimeChangeHandler: function (val) {
 		if (val.length === 5) {
 			// do something with this value
@@ -15,11 +19,14 @@ var TimeWrapper = React.createClass({
 	render: function() {
 		return (
 			<TimeInput
+				name="example"
 				initTime='11:12'
 				ref="TimeInputWrapper"
 				className='s-input -time'
 				mountFocus='true'
 				onTimeChange={this.onTimeChangeHandler}
+				onFocusHandler={this.onFocusHandler}
+				onBlurHandler={this.onBlurHandler}
 			/>
 		);
 	}
